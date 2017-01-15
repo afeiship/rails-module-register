5ff71db5609ac511db272a53108502fbf3035438
afeiship <1290657123@qq.com>
2017-01-15 23:51:39 +0800

    udpte docs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def signup
     @user = User.new
   end
+
+  def create
+    # render plain: params.inspect
+    user = User.create(user_params)
+    user.save
+    redirect_to :root
+  end
+
+  def show
+    
+  end
+
+  private
+    def user_params
+      params.require(:user).permit!
+    end
 end
