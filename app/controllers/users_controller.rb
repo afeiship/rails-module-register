9ac494c37aa6ecd4697ac865977200282ddc9e8c
afeiship <1290657123@qq.com>
2017-03-17 21:06:42 +0800

    udpate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    # render plain: params.inspect
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user
+      redirect_to '/users';
     else
       render :signup
     end
